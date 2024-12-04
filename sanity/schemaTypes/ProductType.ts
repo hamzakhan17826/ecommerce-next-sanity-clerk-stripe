3ffb0bf32b7036c2f577743ca4ccd80d612524fb
Hamza Khan <hamzakhan17826@gmail.com>
2024-12-04 17:21:35 +0500

    working on adding productType

---
+++ sanity/schemaTypes/ProductType.ts
@@ -0,0 +1,17 @@
+import { TrolleyIcon } from "@sanity/icons";
+import { defineField, defineType } from "sanity";
+
+export const productType = defineType({
+  name: "product",
+  title: "Product",
+  type: "document",
+  icon: TrolleyIcon,
+  fields: [
+    defineField({
+      name: "name",
+      title: "Product Name",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+    }),
+  ],
+});
